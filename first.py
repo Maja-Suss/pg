02d93f104b35ae6d7577e1c78ebcae854ec21e95
Maja-Suss <sussom00@jcu.cz>
2025-10-02 13:52:21 +0000

    první program

---
+++ first.py
@@ -0,0 +1,11 @@
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print(f"Číslo {cislo} je sudé")
+    else:
+        print(f"Číslo {cislo} je liché")
+
+
+
+# volání funkce podle zadání
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)

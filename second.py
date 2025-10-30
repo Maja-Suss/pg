968ab7cb0435b693d64308897ba4e04ec82a83fa
Maja-Suss <sussom00@jcu.cz>
2025-10-30 07:51:43 +0000

    dú2

---
+++ second.py
@@ -0,0 +1,9 @@
+def cislo_text(cislo):
+    # funkce zkonvertuje cislo do jeho textove reprezentace
+    # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
+    return "dvacet pět"
+
+if   == "__main_ cislo = input (" 0 ")(" -> ")
+    text = "nula" ("0")
+    print("nula")
+    return "nula"

900924391163efa88e95e22412015e5dfff8a107
Maja-Suss <sussom00@jcu.cz>
2025-11-20 07:57:46 +0000

    dú1

---
+++ first.py
@@ -0,0 +1,14 @@
+# funkce vypise "Číslo X je sudé" pokud je cislo sude a "Číslo X je liché" pokud je cislo liche
+def sudy_nebo_lichy(cislo):
+    if cislo % 2:
+        return "liche"
+    else:
+        return "sude"
+
+def main():
+    promena = 1
+    vysledek = sudy_nebo_lichy(promena)
+    print(vysledek)
+
+if __name__ == "__main__":
+    main()
